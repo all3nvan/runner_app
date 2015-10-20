6152b4eb2eb0500725b903aafeb6220739c078f6
Allen Van <all3nvan@gmail.com>
2015-10-19 19:07:31 -0500

    initial commit

---
+++ Runner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Runner
+//
+//  Created by Allen Van on 10/19/15.
+//  Copyright © 2015 Group9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Runner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Runner
+//
+//  Created by Allen Van on 10/19/15.
+//  Copyright © 2015 Group9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
