b536ef73abfd579043455d600dbff0d28300928f
Souraphath <w.souraphath@gmail.com>
2015-10-28 15:50:46 -0500

    Added MenuViewController.h and .m; this class is connected to the sidemenu view in the storyboard.

---
+++ Runner/MenuViewController.h
@@ -0,0 +1,14 @@
+//
+//  MenuViewController.h
+//  Runner
+//
+//  Created by abc on 10/28/15.
+//  Copyright © 2015 Group9. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MenuViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *settingsImage;
+
+@end
